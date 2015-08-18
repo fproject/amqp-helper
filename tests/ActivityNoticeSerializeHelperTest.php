2f6f240256135d5dbfb415d1aef6b80cd1fa1cc8
Bui Sy Nguyen <nguyenbs@projectkit.net>
2015-08-18 23:45:16 +0700

    Update test cases

--- tests/ActivityNoticeSerializeHelperTest.php
+++ tests/ActivityNoticeSerializeHelperTest.php
@@ -1,11 +1,12 @@
 <?php
 use fproject\amqp\ActivityNoticeSerializeHelper;
 include_once('TestModel01.php');
+include_once('TestModel02.php');
 class ActivityNoticeSerializeHelperTest extends PHPUnit_Framework_TestCase
 {
     private $params = [];
 
-    public function testPushAndPop()
+    public function testGetSerializeData01()
     {
         $this->params = [
             'activityNotice' => [
@@ -33,6 +34,45 @@ class ActivityNoticeSerializeHelperTest extends PHPUnit_Framework_TestCase
         $this->assertArrayHasKey('field2',$data);
         $this->assertArrayHasKey('field1',$data);
         $this->assertInstanceOf('DateTime',$data['field1']);
+    }
+
+    public function testGetSerializeData02()
+    {
+        $this->params = [
+            'activityNotice' => [
+                '*' => [
+                    'notSerializeAttributes' => '_explicitType'
+                ],
+                'testModel02' => [
+                    'notifyActions' => [//Use '*' to indicate all actions will be applied
+                        'delete' => ['serializeAttributes' => 'id'],
+                        'add',//Use simple string value to indicate all attributes will be applied
+                        'update' => [
+                            //'listenAttributes' => '*',
+                            'notListenAttributes' => 'jsonData',
+                            //'serializeAttributes' => '*',
+                            'notSerializeAttributes' => 'jsonData,group,workCalendar,resources,projectTasks'
+                        ]
+                    ]
+                ],
+            ]
+        ];
+
+        $helper = new ActivityNoticeSerializeHelper($this->params);
+        $model = new TestModel02();
+        $model->id = '001';
+        $model->endTime = new DateTime();
+        $model->jsonData = '{}';
+        $model->name = 'Name 001';
+        $model->_explicitType = "TestModel02";
+
+        $config = $helper->getActivityNoticeConfig('testModel02', 'update', null);
+        $data = $helper->getSerializeData($model, $config);
+        $this->assertArrayNotHasKey('_explicitType',$data);
+        $this->assertArrayNotHasKey('group',$data);
+        $this->assertArrayHasKey('id',$data);
+        $this->assertArrayHasKey('endTime',$data);
+        $this->assertInstanceOf('DateTime',$data['endTime']);
     }
 }
 ?>
---
+++ tests/TestModel02.php
@@ -0,0 +1,66 @@
+<?php
+///////////////////////////////////////////////////////////////////////////////
+//
+// Licensed Source Code - Property of ProjectKit.net
+//
+// © Copyright ProjectKit.net 2015. All Rights Reserved.
+//
+///////////////////////////////////////////////////////////////////////////////
+/* ****************************************************************************
+ *
+ * This class is automatically generated and maintained by Gii, be careful
+ * when modifying it.
+ *
+ * Your additional properties and methods should be placed at the bottom of
+ * this class.
+ *
+ *****************************************************************************/
+/**
+ * This is the ProjectKit.net VO class associated with the model "Project".
+ */
+class TestModel02
+{
+    /**
+     * Map the ActionScript class that has alias 'FProject' to this VO class:
+     */
+    public $_explicitType = 'TestModel02';
+
+    /** @var integer $id */
+    public $id;
+
+    /** @var string $code */
+    public $code;
+
+    /** @var string $name */
+    public $name;
+
+    /** @var integer $type */
+    public $type;
+
+    /** @var string $description */
+    public $description;
+
+    /** @var DateTime $startTime */
+    public $startTime;
+
+    /** @var DateTime $endTime */
+    public $endTime;
+
+    /** @var mixed $jsonData */
+    public $jsonData;
+
+    /** @var integer $status */
+    public $status;
+
+    public $group;
+
+    public $issueWorkflow;
+
+    public $projectSummaryTask;
+
+    public $workCalendar;
+
+    public $resources;
+
+    public $projectTasks;
+}
