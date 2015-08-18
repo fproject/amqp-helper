7d4daf142b492a7734326b822093b4278c6f61ee
Bui Sy Nguyen <nguyenbs@projectkit.net>
2015-08-18 22:44:47 +0700

    Add PHPUnit test cases

--- tests/ActivityNoticeSerializeHelperTest.php
+++ tests/ActivityNoticeSerializeHelperTest.php
@@ -19,17 +19,17 @@ class ActivityNoticeSerializeHelperTest extends PHPUnit_Framework_TestCase
             ]
         ];
 
-
         $helper = new ActivityNoticeSerializeHelper($this->params);
         $model = new TestModel01();
         $model->field1 = new DateTime();
         $model->field2 = 'ABC';
         $model->field3 = 123;
         $config = $helper->getActivityNoticeConfig('testModel01', 'add', null);
-        echo print_r($config,true);
         $data = $helper->getSerializeData($model, $config);
-        echo print_r($data,true);
-        $this->assertTrue(false);
+        $this->assertArrayNotHasKey('field3',$data);
+        $this->assertArrayHasKey('field2',$data);
+        $this->assertArrayHasKey('field1',$data);
+        $this->assertInstanceOf('DateTime',$data['field1']);
     }
 }
 ?>
