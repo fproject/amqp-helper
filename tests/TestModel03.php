1c0c03342b41a75340aa57bd1b8a4a186fb3c43f
Bui Sy Nguyen <nguyenbs@projectkit.net>
2016-02-26 17:37:44 +0700

    Testdata for #3

--- tests/TestModel03.php
+++ tests/TestModel03.php
@@ -3,4 +3,10 @@ class TestModel03 extends TestModel01
 {
     public $myField1;
     public $myField2;
+
+    public function mySerializeDelegateFunction($config)
+    {
+        $this->field1 = "@@@";
+        return $this;
+    }
 }
